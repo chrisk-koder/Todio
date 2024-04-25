76d1fe791b55d272c3c7a3c2721ee4e83b1870e2
Chris <chris.koder@gmail.com>
2024-04-25 16:13:00 +0200

    Button & checkbox styling, Save & load data, edit task, delete task, task UUID
    + Added new styling for checkbox and buttons
    + Added new save and load data feature, saves tasks with unique IDs and then stores information about task in it
    + Added create task elements function and moved most of code from add task function into this so it can be used for creating new tasks and loading tasks
    + Added task name editing, clicking on the task name will show an input where you can type in your new name and press enter to apply. (WIP)
    ~ Fixed mark as complete function to actually mark it as complete and save it so it persists on reload

--- dist/js/todoList.js
+++ dist/js/todoList.js
@@ -9,6 +9,41 @@ window.addEventListener("load", (event) => {
     loadData();
 });
 
+// Save data function
+function saveData(taskID, taskData) {
+
+    // Does not save the state of the checkbox yet
+    localStorage.setItem(taskID, JSON.stringify(taskData));
+    console.log("Data saved");
+
+}
+
+// Load data function
+function loadData() {
+
+    for (let i = 0; i < localStorage.length; i++) {
+        const taskID = localStorage.key(i);
+        const taskData = JSON.parse(localStorage.getItem(taskID));
+        
+        const taskName = taskData.taskName;
+        const isChecked = taskData.isChecked;
+
+        createTaskElements(taskID, taskName, isChecked);
+    }
+
+    checkIfListIsEmpty();
+    console.log("Tasks retrieved");
+}
+
+function deleteData(taskID) {
+    localStorage.removeItem(taskID);
+}
+
+function generateUUID() {
+    return Math.random().toString(16).slice(2);
+}
+
+// ------------------------------------------------------------------ //
 
 // Checks if list is empty, if it is, show empty text
 function checkIfListIsEmpty() {
@@ -34,50 +69,90 @@ todoUL.addEventListener("click", function(event) {
     }
 });
 
-todoUL.addEventListener("click", function(event) {
+/*todoUL.addEventListener("click", function(event) {
     if (event.target && event.target.matches('.editBtn')) {
         //removeTask(event);
     }
+});*/
+
+
+
+todoUL.addEventListener("click", function(event) {
+    if (event.target && event.target.matches('.taskTitle')) {
+        console.log("Clicked title");
+        editTask(event);
+    }
 });
+
+// ------------------------------------------------------------------ //
+
+// Create task element function, a function used when creating tasks and loading them back.
+function createTaskElements(taskID, taskName, isChecked) {
+    
+    // Create the list element
+    const task = document.createElement("li");
+    if (isChecked) {
+        task.className = "grid checked";
+    } else {
+        task.className = "grid";
+    }
+    task.id = taskID;
+
+    // Create the checkbox
+    const checkBox = document.createElement("input");
+    checkBox.checked = isChecked;
+    checkBox.type = "checkbox";
+    checkBox.className = "checkBox";
+
+    // Create the paragraph element (the task name)
+    const title = document.createElement("p");
+    title.className = "taskTitle";
+    title.textContent = taskName;
+
+    // Create the Edit button -- Unused
+    /*const editButton = document.createElement("button");
+    editButton.className = "editBtn btn";
+    editButton.textContent = "Edit";*/
+
+    // Create the Remove button
+    const removeButton = document.createElement("button");
+    removeButton.className = "removeBtn btn hoverBtn";
+    removeButton.textContent = "Remove";
+    
+    // Append the elements
+    task.appendChild(checkBox);
+    task.appendChild(title);
+    //task.appendChild(editButton);
+    task.appendChild(removeButton);
+
+    // Append the task to the list
+    todoUL.appendChild(task);
+
+}
 
 // Add task function
 function addTask() {
     const inputField = document.querySelector(".inputField input");
     const inputValue = inputField.value.trim();
+
+    // Unique Task ID
+    const taskID = generateUUID();
+    const isChecked = false;
     
     // Make sure it's not empty before adding task
     if (inputValue != "") {
-        // Create the list element
-        const task = document.createElement("li");
-
-        // Create the checkbox
-        const checkBox = document.createElement("input");
-        checkBox.type = "checkbox";
-        checkBox.className = "checkBox";
-
-        // Create the paragraph element (the task name)
-        const title = document.createElement("p");
-        title.textContent = inputValue;
-
-        // Create the Edit button
-        const editButton = document.createElement("button");
-        editButton.className = "editBtn btn";
-        editButton.textContent = "Edit";
-
-        // Create the Remove button
-        const removeButton = document.createElement("button");
-        removeButton.className = "removeBtn btn";
-        removeButton.textContent = "Remove";
         
-        // Append the elements
-        task.appendChild(checkBox);
-        task.appendChild(title);
-        task.appendChild(editButton);
-        task.appendChild(removeButton);
+        // Use create task elements function to create the elements
+        createTaskElements(taskID, inputValue, isChecked);
 
-        // Append the task to the list
-        todoUL.appendChild(task);
-        saveData();
+        inputField.value = "";
+
+        // Save the data
+        const taskData = {
+            isChecked: isChecked,
+            taskName: inputValue
+        }
+        saveData(taskID, taskData);
 
         // Check if list is empty (temporary)
         checkIfListIsEmpty();
@@ -87,37 +162,74 @@ function addTask() {
 
 // Remove task function
 function removeTask(event) {
-    const task = event.target.closest("li");
+    const task = event.target.parentElement;
+    const taskID = task.id;
     task.remove();
+
     checkIfListIsEmpty();
-    console.log("Remove");
-    saveData();
+    deleteData(taskID);
 }
 
-// Mark task as complete. (Assigns the class checked to the task, that's it for now)
+// Mark task as complete
 function markAsComplete(event) {
-    const task = event.target.closest("li");
-    if (task.className != "checked") {
+    const task = event.target.parentElement;
+    const taskID = task.id;
+    const taskData = JSON.parse(localStorage.getItem(taskID));
+
+    let updatedData;
+
+    if (!taskData.isChecked) {
         task.classList.add("checked");
+        updatedData = {
+            isChecked: true,
+            taskName: taskData.taskName
+        }
     } else {
         task.classList.remove("checked");
+        updatedData = {
+            isChecked: false,
+            taskName: taskData.taskName
+        }
     }
-    saveData();
-}
-
-// Save data function
-function saveData() {
-
-    // Does not save the state of the checkbox yet
-    localStorage.setItem("taskList", todoUL.innerHTML);
-    console.log("Data saved");
-}
-
-// Load data function
-function loadData() {
-    todoUL.innerHTML = localStorage.getItem("taskList");
-    checkIfListIsEmpty();
-    console.log("Tasks retrieved");
+    saveData(taskID, updatedData);
 }
 
 
+// Editing task function
+// Allows the user to click on the paragraph element to edit the name of the task,
+// Replaces the paragraph with an input that the user can enter the new name into and press enter to apply the changes
+function editTask(event) {
+    const paragraph = event.target;
+    const task = paragraph.parentElement;
+    const taskID = task.id;
+    console.log(paragraph);
+    const taskData = JSON.parse(localStorage.getItem(taskID));
+    
+    // Create the input element
+    const editInput = document.createElement("input");
+    editInput.type = "text"; // Set the input type as needed
+    editInput.value = paragraph.textContent; // Set the input value to the paragraph's text content
+    editInput.id = "editTaskInput";
+
+    // Replace the paragraph with the input element
+    task.replaceChild(editInput, paragraph);
+    editInput.focus();
+
+    editInput.addEventListener("keyup", function(event) {
+        if (event.key === "Enter") {
+            
+            const newName = editInput.value;
+            task.replaceChild(paragraph, editInput);
+            paragraph.textContent = newName;
+            
+            // Save the updated data
+            const updatedData = {
+                isChecked: taskData.isChecked,
+                taskName: newName
+            }
+            saveData(taskID, updatedData);
+        }
+    });
+
+    
+}
